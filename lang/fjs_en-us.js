e2db69955106e07df676071b8b54ec8c28495d56
Yuriy Zisin <zysoftik@gmail.com>
2011-12-12 23:26:14 +0200

    Added examples for facebook,lang, and forms&validation plugins. Some bugfixes

---
+++ lang/fjs_en-us.js
@@ -0,0 +1,8 @@
+$$.fjs.lang.add('en-us', {
+    'Localization test'                   : 'Localization test',
+    'Select language'                     : 'Select language',
+    'This string translates immediately'  : 'This string translates immediately',
+    'Runtime localization'                : 'Runtime localization',
+    'Click me'                            : 'Click me',
+    'This is a test'                      : 'This is a test'
+});
---
+++ lang/fjs_ru-ru.js
@@ -0,0 +1,8 @@
+$$.fjs.lang.add('ru-ru', {
+    'Localization test'                   : 'Тест локализации',
+    'Select language'                     : 'Выберите язык',
+    'This string translates immediately'  : 'Эта строка переводится моментально',
+    'Runtime localization'                : 'Локализация из JavaScript',
+    'Click me'                            : 'Кликни меня',
+    'This is a test'                      : 'Это тест'
+});
